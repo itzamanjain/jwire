24155e010d09335e34b00df8664698ed1ca4be80
Aman Jain <145151053+itzamanjain@users.noreply.github.com>
2024-03-26 18:21:04 +0530

    Update Footers.jsx (navlink)

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -75,12 +75,16 @@ const Footer = () => {
               </NavLink>
             </li>
             <li>
-              <a
-                href="/products"
-                className="hover:text-gray-400 transition duration-300"
-              >
-                Product
-              </a>
+             <NavLink
+                  to="/products"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${
+                      isActive ? "text-blue-700" : "text-gray-700"
+                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-700 lg:p-0`
+                  }
+                >
+                  Products
+                </NavLink>
             </li>
           </ul>
         </div>

f934c8917213050a3dfc21793968fce602c5cb0e
Arihant jain <138019782+arihantdotcom@users.noreply.github.com>
2024-03-26 11:23:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { createRoot } from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import "./index.css";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home.jsx";
@@ -10,27 +11,23 @@ import Project from "./components/Project.jsx";
 import Contact from "./components/Contact.jsx";
 import CareerPage from "./components/Career.jsx";
 
-const App = () => {
-  return (
- 
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Product />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/career" element={<CareerPage />} />
-      </Routes>
-      
-    </Router>
-  );
-};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="products" element={<Product />} />
+      <Route path="project" element={<Project />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="career" element={<CareerPage />} />
+    </Route>
+  )
+)
 
-createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
